ca9587c28ed314b6d82891b777734c9e4b0fdfdb
Jorge Mario Lasagni <jlasagni@gmail.com>
2022-11-09 13:51:57 -0300

    Listo para Maquetear

--- procedimientos/prestamo.js
+++ procedimientos/prestamo.js
@@ -1,72 +1,175 @@
-//Procedimiento para simulación de PRESTAMOS Sistema Alemán
+//Procedimiento para simulación de PRESTAMOS Sistema Alemán o Francés
 function genero_cuotas(){
-    //alert("Entré a Genero cuotas")
+    //Inicializo la tabla
     document.getElementById("tab").innerHTML="";
+    //Transformo a numéricos los valores cargados en html
+    let s=Number(document.getElementById("sistema").value);
     let v=Number(document.getElementById("capital").value);
     let i=Number(document.getElementById("interes").value);
-    let m=Number(document.getElementById("tiempo").value);
-    let t=12
-
-    //alert("Capital:"+v);
-    //alert("Interés:"+i);
-    //alert("Cuotas:" +m);
-
-    //-----------------------------------------------------------------
-    //Validaciones...
-    if (v === 0) {
-        return alert("Debe ingresar CAPITAL mayor a 0!");
+    let p=Number(document.getElementById("plazo").value);
+    let f=Number(document.getElementById("frecuencia").value);
+    //------------------------------------------------------------------
+    //Validación de la carga...
+    //funcion "valido" para control de la carga
+    //s:sistema v:capital i:interés p:plazo f:frecuencia
+    mensaje = valido(s,v,i,p,f)
+    if (mensaje != ""){
+        return alert(mensaje);
     }
-    if (m <= 0) {
-        return alert("Debe ingresar número de CUOTAS mayor a 0!");
-    }
-    if (m > 24) {
-        return alert("El número de CUOTAS no puede superar las 24!");
-    }
-    //-----------------------------------------------------------------
+    //------------------------------------------------------------------
     //Armo la tabla
-    let cct = 0.00
-    let cit = 0.00
-    let ctt = 0.00
+    //Sumarizadores para mostrar los Totales
+    let cct = 0.00      //cc Total cuota capital
+    let cit = 0.00      //ci Total cuota interés
+    let ctt = 0.00      //ct Total cuota total 
 
-    for(n = 1; n <= m; n++){
-        ca = v - (n-1) * v / m;
-        c1=ca.toFixed(2);
+    //Valores intermedios necesarios para los cálculos
+    let cpa   = 12 / f    //cantidad de pagos por año
+    let ieq   = i / cpa   //interés equivalente por período de pago
+    let m     = p * cpa   //Cantidad de pagos en el plazo anual
+    let ca    = v         //Capital Vivo!!
 
-        cc = v / m;
-        c2=cc.toFixed(2);
-
-        //ci = ca * i * (m-n+1) / (100 * t);
-        //c3=ci.toFixed(2);
-        ci = ca * ieq / 100
-        c3=ci.toFixed(2);
-
-        ct = (cc + ci);
-        c4=ct.toFixed(2);
-
+    //Sistema Alemán  1 (arranco el "for en 1")
+    //Sistema Francés 2 (arranco el "for" en 0)
+    let desde = 0         //Inicio del for distinto para cada sistema
+    switch(s){
+        case 1:
+            desde = 1;
+            break
+        case 2:
+            desde = 0;    
+            break 
+    }
+    //------------------------------------------------------------------
+    for(n = desde; n <= m; n++){
+        switch(s){
+            case 1:
+                //Capital
+                ca = v - (n-1) * v / m;               
+                //Cuota Capital
+                cc = v / m;
+                //Cuota Interés (amortización)
+                ci = ca * ieq / 100;
+                //Cuota Total
+                ct = (cc + ci);
+                break;
+            case 2:
+                if (n > 0){
+                    //Cuota Total
+                    ct = (v * (i/100)) / (1-(potencia((1+(i/100)),-p)));
+                    //Cuota Interés (amortización)
+                    ci = ca * ieq / 100;
+                    //Cuota Capital
+                    cc = ct - ci;
+                    //Capital
+                    ca = ca - cc;
+                } else{
+                    //Cuota Total
+                    //a  = 0;
+                    ct = 0;
+                    //Cuota Interés (amortización)
+                    ci = 0;
+                    //Cuota Capital
+                    cc = 0;
+                    //Capital
+                    ca = v;
+                }
+                break;
+        }       
+        //Sumarizo los Totales por columnas
         cct = cct + cc;
         cit = cit + ci;
         ctt = ctt + ct;
-        
+        //Preparo los parciales para la tabla
+        c1=ca.toFixed(0);
+        c2=cc.toFixed(0);
+        c3=ci.toFixed(0);
+        c4=ct.toFixed(0);
+        //Cargo la fila en la tabla
         document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
-        `<tr class="table-primary">
-            <th scope="row">${n}</th>
-            <td> ${c1} </td>
-            <td> ${c2} </td>
-            <td> ${c3} </td>
-            <td> ${c4} </td>
-        </tr>`;
+            `<tr class="table-primary">
+                <th scope="row">${n}</th>
+                <td> ${c1} </td>
+                <td> ${c2} </td>
+                <td> ${c3} </td>
+                <td> ${c4} </td>
+            </tr>`;
     }
-    //Preparo los totales
-    ct2=cct.toFixed(2);
-    ct3=cit.toFixed(2);
-    ct4=ctt.toFixed(2);
+    //------------------------------------------------------------------
+    //Cargo los totales a la tabla
+    //Paso los totales a dos decimales
+    ct2=cct.toFixed(0);
+    ct3=cit.toFixed(0);
+    ct4=ctt.toFixed(0);
+    //Cargo los totales en la tabla
     document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
-    `<tr class="table-dark">
-        <th scope="row">Total</th>
-        <td>        </td>
-        <td> ${ct2} </td>
-        <td> ${ct3} </td>
-        <td> ${ct4} </td>
-    </tr>`;
+        `<tr class="table-dark">
+            <th scope="row">Total</th>
+            <td>        </td>
+            <td> ${ct2} </td>
+            <td> ${ct3} </td>
+            <td> ${ct4} </td>
+        </tr>`;
 }
 //----------------------------------------------------------------------
+//----------------------------------------------------------------------
+function valido(ps,pv,pi,pp,pf){
+   //Sistema
+    if (ps!=1 && ps!=2){
+        return "El sistema de cálculo debe ser Alemán o Francés"
+    } 
+    //Capital
+    if (pv <= 0){
+        return "El Capital solicitado debe ser mayor a $0"
+    } 
+    //Interés o tasa anual
+    if (pi <= 0){
+        return "El Interés o Tasa Anual debe ser positiva"
+    } 
+    //Plazo de pago
+    if (pp <= 0){
+        return "El Plazo de pago debe ser al menos por un año"
+    } 
+    //Frecuencia de pago
+    switch(pf){
+        case 1:
+            break;
+        case 2:
+            break;  
+        case 3:
+            break;
+        case 4:
+            break; 
+        case 6:
+            break;
+        case 12:
+            break;         
+        default:
+            return "La Frecuencia de pago debe se mensual, bimestral, trimestral, cuatrimestral, semestral o anual!";
+    }
+    return "";
+}
+//----------------------------------------------------------------------
+//La función potencia es más clara utilizando el "for" 
+// uso el while para aplicar lo visto.
+//Si el exponente es (-) lo multipico por -1 (o debería tomar el valor absoluto)
+// para después obtener como resultado la inversa (1/x). 
+//Si el exponente es positivo el resultado es x
+function potencia (base, exponente){
+    let exp = exponente;
+    if (exp < 0){
+        exp = exp * (-1);
+    }
+    let res = 1;
+    let pot = 1;
+
+    while(pot <= exp){
+        res = res * base;
+        pot++;
+    }
+    if (exponente < 0){
+        res = 1 / res;
+    }
+    return res
+}
+//Fin del JS------------------------------------------------------------
--- procedimientos/pruebas.js
+++ procedimientos/pruebas.js
@@ -1,97 +1,146 @@
 function genero_cuotas(){
-    //alert("Entré a Genero cuotas")
+    //Inicializo la tabla
     document.getElementById("tab").innerHTML="";
-    
+    //Transformo a numéricos los valores cargados en html
     let s=Number(document.getElementById("sistema").value);
     let v=Number(document.getElementById("capital").value);
     let i=Number(document.getElementById("interes").value);
     let p=Number(document.getElementById("plazo").value);
     let f=Number(document.getElementById("frecuencia").value);
-//alert("1")
-    //let k=Number(document.getElementById("flexRadioDefault1").value);
-    //let l=Number(document.getElementById("flexRadioDefault2").value);
-    //alert("k:"+k);
-    //alert("l:"+l);
-    //v   = 3937.50
-    //i   = 2.15
-    //m   = 10
-    //t   = 12
-    //alert("Capital:"+v);
-    //alert("Interés:"+i);
-    //alert("Cuotas:" +m);
-    //alert("Sistema:" +s);
-    //-----------------------------------------------------------------
-    //Validaciones...
+    //------------------------------------------------------------------
+    //Validación de la carga...
+    //funcion "valido" para control de la carga
+    //s:sistema v:capital i:interés p:plazo f:frecuencia
+    //s=2
+    //v=10000
+    //i=3
+    //p=5
+    //f=12
     mensaje = valido(s,v,i,p,f)
     if (mensaje != ""){
-         return alert(mensaje);
+        return alert(mensaje);
     }
-alert("todo OK!")
-    //if(s<>1 && s<>2){3
-    //}
-    //if (v === 0) {
-    //    return alert("Debe ingresar CAPITAL mayor a 0!");
-    //}
-    //if (m <= 0) {
-    //    return alert("Debe ingresar número de CUOTAS mayor a 0!");
-    //}
-    //if (m > 24) {
-    //    return alert("El número de CUOTAS no puede superar las 24!");
-    //}
-    //if (s!=1 && s!=2){
-    //    return alert("El seleccionat un sistema");
-    //    //    mensaje = "El sistema de cálculo debe ser Alemán o Francés"
-    //    //    return //false
-    //    } 
-    //-----------------------------------------------------------------
+    //alert("sali de las validaciones");
+    //------------------------------------------------------------------
     //Armo la tabla
-    let cct = 0.00
-    let cit = 0.00
-    let ctt = 0.00
-    let ieq = i / t
+    //Sumarizadores para mostrar los Totales
+    let cct = 0.00      //cc Total cuota capital
+    let cit = 0.00      //ci Total cuota interés
+    let ctt = 0.00      //ct Total cuota total 
 
-    for(n = 1; n <= m; n++){
+    //Valores intermedios necesarios para los cálculos
+    let cpa = 12 / f    //cantidad de pagos por año
+    let ieq = i / cpa   //interés equivalente por período de pago
+    let m   = p * cpa   //Cantidad de pagos en el plazo anual
+    let ca  = v         //Capital Vivo!!
+    let desde = 0
+    //alert("por entrar al switch");
+    //Sistema Alemán  1 (arranco el "for en 1")
+    //Sistema Francés 2 (arranco el "for" en 0)
+    switch(s){
+        case 1:
+            //alert("1 por entrar al switch");       
+            desde = 1;
+            break
+        case 2:
+            //alert("2 ?? por entrar al switch");       
+            desde = 0;    
+            break 
+    }
+    //------------------------------------------------------------------
+    //alert("por entrar al for")        
 
-        ca = v - (n-1) * v / m;
-        c1=ca.toFixed(2);
+    for(n = desde; n <= m; n++){
+        //alert("en for con n:"+n);
+        switch(s){
+            case 1:
+                //Capital
+                ca = v - (n-1) * v / m;               
+                //Cuota Capital
+                cc = v / m;
+                //Cuota Interés (amortización)
+                ci = ca * ieq / 100;
+                //Cuota Total
+                ct = (cc + ci);
+                break;
+            case 2:
+                //alert("case 2...n:"+n)
+                if (n > 0){
+                    //Cuota Total
+                    ct = (v * (i/100)) / (1-(potencia((1+(i/100)),-p)));
 
-        cc = v / m;
-        c2=cc.toFixed(2);
+                    //alert("a:"+a)
+                    //ct = v * (i/100);
+                    //alert("ct:"+ct)
+                    //loQue = 1+(i/100);
+                    //alert("loQue:"+loQue)
+                    //loQue = potencia(loQue,-p);
+                    //alert("despues de potencia:"+loQue)
+                    //ct = ct / (1-loQue);
+                    //alert("ct:"+ct)
 
-        //ci = ca * i * (m-n+1) / (100 * t);
-        //c3=ci.toFixed(2);
-        ci = ca * ieq / 100
-        c3=ci.toFixed(2);
-
-        ct = (cc + ci);
-        c4=ct.toFixed(2);
-
+                    //Cuota Interés (amortización)
+                    ci = ca * ieq / 100;
+                    //Cuota Capital
+                    cc = ct - ci;
+                    //Capital
+                    ca = ca - cc;
+                } else{
+                    //Cuota Total
+                    //a  = 0;
+                    ct = 0;
+                    //Cuota Interés (amortización)
+                    ci = 0;
+                    //Cuota Capital
+                    cc = 0;
+                    //Capital
+                    ca = v;
+                }
+                //alert("n:"+n+" ct:"+ct.toFixed(2)+" ci:"+ci.toFixed(2)+" cc:"+cc.toFixed(2)+" ca:"+ca.toFixed(2))
+                break;
+        }       
+        //Sumarizo los Totales por columnas
+        //alert("por sumarizar 1")
         cct = cct + cc;
         cit = cit + ci;
         ctt = ctt + ct;
-        
+        //Preparo los parciales para la tabla
+       // alert("por sacar decimales")
+
+        c1=ca.toFixed(2);
+        c2=cc.toFixed(2);
+        c3=ci.toFixed(2);
+        c4=ct.toFixed(2);
+        //alert(21)
+        //Cargo la fila en la tabla
         document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
-        `<tr class="table-primary">
-            <th scope="row">${n}</th>
-            <td> ${c1} </td>
-            <td> ${c2} </td>
-            <td> ${c3} </td>
-            <td> ${c4} </td>
-        </tr>`;
+            `<tr class="table-primary">
+                <th scope="row">${n}</th>
+                <td> ${c1} </td>
+                <td> ${c2} </td>
+                <td> ${c3} </td>
+                <td> ${c4} </td>
+            </tr>`;
+            //alert(2)
     }
-    //Preparo los totales
+    //------------------------------------------------------------------
+    //Cargo los totales a la tabla
+    //Paso los totales a dos decimales
     ct2=cct.toFixed(2);
     ct3=cit.toFixed(2);
     ct4=ctt.toFixed(2);
+    //Cargo los totales en la tabla
     document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
-    `<tr class="table-dark">
-        <th scope="row">Total</th>
-        <td>        </td>
-        <td> ${ct2} </td>
-        <td> ${ct3} </td>
-        <td> ${ct4} </td>
-    </tr>`;
+        `<tr class="table-dark">
+            <th scope="row">Total</th>
+            <td>        </td>
+            <td> ${ct2} </td>
+            <td> ${ct3} </td>
+            <td> ${ct4} </td>
+        </tr>`;
 }
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
 function valido(ps,pv,pi,pp,pf){
    //Sistema
     if (ps!=1 && ps!=2){
@@ -112,20 +161,55 @@ function valido(ps,pv,pi,pp,pf){
     //Frecuencia de pago
     switch(pf){
         case 1:
-            break
+            break;
         case 2:
-            break  
+            break;  
         case 3:
-            break
+            break;
         case 4:
-            break 
+            break; 
         case 6:
-            break
+            break;
         case 12:
-            break         
+            break;         
         default:
-            return "La Frecuencia de pago debe se mensual, bimestral, trimestral, cuatrimestral, semestral o anual!"
+            return "La Frecuencia de pago debe se mensual, bimestral, trimestral, cuatrimestral, semestral o anual!";
     }
-    return ""
+    return "";
 }
 //----------------------------------------------------------------------
+//La función potencia es más clara utilizando el "for" 
+// uso el while para aplicar lo visto.
+//Si el exponente es (-) lo multipico por -1 (o debería tomar el valor absoluto)
+// para después obtener como resultado la inversa (1/x). 
+//Si el exponente es positivo el resultado es x
+function potencia (base, exponente){
+    //alert("estoy en potencia");
+    let exp = exponente;
+    //alert("exponente:"+exponente+"/exp:"+exp);
+    //alert("Base:"+base)
+
+    if (exp < 0){
+        exp = exp * (-1);
+    }
+    //alert("como exponente es negativo:"+exponente+"/exp:"+exp);
+
+    let res = 1;
+    let pot = 1;
+
+    while(pot <= exp){
+
+        res = res * base;
+        //alert(":"+pot+" res"+res);
+
+        pot++;
+    }
+    //alert("Resultado de la potencia sin invertir:"+res)
+
+    if (exponente < 0){
+        res = 1 / res;
+    }
+    //alert("Resultado de la potencia YA invertido:"+res)
+    return res
+}
+//Fin del JS------------------------------------------------------------
